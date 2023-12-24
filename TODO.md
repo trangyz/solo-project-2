44e9b5dda45739c47a574763a34ccc443ba49c35
Trang <trang.do@chicagobooth.edu>
2023-12-25 08:13:29 +1000

    working on front end

---
+++ TODO.md
@@ -0,0 +1,6 @@
+## To do
+
+[ ] Render current accounts
+[ ] Update post logic
+[ ] Authenticate 
+[ ] Add sign out?
--- client/App.jsx
+++ client/App.jsx
@@ -1,21 +1,10 @@
-
 import React from 'react';
+import Inputs from './components/Inputs.jsx';
 
-const App = () => {
+const App = (props) => {
     return (
-        <div>
-            <form method="POST" action="/update">
-                <div>Current accounts</div>
-                <input name="accounts" type="text" placeholder="$3000"></input>
-
-                <div>Planned monhtly savings</div>
-                <input name="savings" type="text" placeholder="$200"></input>
-
-                <div>Planned retirement age</div>
-                <input name="retirement" type="text" placeholder="60"></input>
-
-                <div><input type="submit" value="Calculate"></input></div>
-            </form>
+        <div className="column">
+            <Inputs />
         </div>
 
     )

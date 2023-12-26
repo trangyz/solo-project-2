ba64d0a12bc98c6f876ac424cf2127bd15e708af
Trang <trang.do@chicagobooth.edu>
2023-12-26 22:58:58 +1000

    restyling

--- TODO.md
+++ TODO.md
@@ -6,3 +6,7 @@
 [ ] Update future wealth calculation logic
 [ ] Clean up authentication 
 [ ] Add sign out?
+
+## Challenges
+
+Authentication and JSON parsing. Tried to save the user object as a property in the cookie, and then tried to send that object as a response, and spent a long time trying to figure out why I couldn't parse the JSON response into the front end  
--- client/components/Inputs.jsx
+++ client/components/Inputs.jsx
@@ -100,20 +100,53 @@ class Inputs extends Component {
                 </div>
             ))
             : <div>Loading accounts...</div>;
+
+        const newAccountForm = () => {
+            return (
+                <div className='account-card'>
+                    <div>Account name</div>
+                    <input
+                        name="account_name"
+                        type="text"
+                        // placeholder={props.account_name}
+                        // value={accountName}
+                        // onChange={(e) => setAccountName(e.target.value)}
+                    />
+
+                    <div>Balance</div>
+                    <input
+                        name="balance"
+                        type="text"
+                        // placeholder={props.balance}
+                        // value={balance}
+                        // onChange={(e) => setBalance(e.target.value)}
+                    />
+
+                    <div>Annual return</div>
+                    <input
+                        name="annual_return"
+                        type="text"
+                        // placeholder={props.annual_return}
+                        // value={annualReturn}
+                        // onChange={(e) => setAnnualReturn(e.target.value)}
+                    />
+                    <button>Add account</button>
+                </div>
+            )
+        }
+
         if (!this.state.user) {
             return (
                 <div>Loading...</div>
             )
         } else {
-            console.log(`rendering while state is ${this.state.user.username}`)
 
             return (
                 <div>
                     <form onSubmit={this.handleSubmit}>
 
-                        <div>Current accounts</div>
                         {accountsElems}
-
+                        {newAccountForm()}
                         <div>Planned monthly savings</div>
                         <input name="monthly_savings" type="text" placeholder={this.state.user.monthly_savings}></input>
 
